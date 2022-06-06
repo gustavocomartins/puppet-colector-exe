98eaf95a6ba380f800431d646dae31663551c694
Victor Linhati <VICTOR.LINHATI@bandtec.com.br>
2022-06-06 19:23:33 -0300

    Slack

---
+++ puppet-collector/src/main/java/com/mycompany/puppet/colector/Example.java
@@ -0,0 +1,13 @@
+package com.mycompany.puppet.colector;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.response.api.ApiTestResponse;
+
+public class Example {
+
+    public static void main(String[] args) throws Exception {
+        Slack slack = Slack.getInstance();
+        ApiTestResponse response = slack.methods().apiTest(r -> r.foo("bar"));
+        System.out.println(response);
+    }
+}
